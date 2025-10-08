b7d496582ed61e9b2f52822a9d93052b29050cd0
SereneParadox <datboiju01@gmail.com>
2025-10-07 20:35:33 -0700

    Implement Ball Movement

--- pong-along/Ball.cs
+++ pong-along/Ball.cs
@@ -3,5 +3,17 @@ using System;
 
 public partial class Ball : CharacterBody2D
 {
-    [Export] public float speed = 400f;
+	[Export] public float speed = 400f;
+	private Vector2 _direction = new Vector2(1, 1).Normalized();
+	public override void _Ready()
+	{
+		//random direction ball launch
+		float angle = (float)GD.RandRange(-.5, .5);
+		_direction = new Vector2(1, angle).Normalized();
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		Velocity = _direction * speed;
+		MoveAndSlide();
+	}
 }
